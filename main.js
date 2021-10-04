39ea1e6ada02cf3969e55e7b35c9d745d3da267d
charonyuu <bensona1219@gmail.com>
2021-10-04 08:35:59 +0800

    css

---
+++ main.js
@@ -0,0 +1,12 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('li');
+    paragraph.classList.add('li');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+})
